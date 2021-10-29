c2275f0d8b428fe1da2b1d7006d732238b2c73da
syedfakhar25 <syedfakhar25@gmail.com>
2021-10-29 16:23:00 +0500

    accept reject EMAIK

--- resources/views/mail/student/accept.blade.php
+++ resources/views/mail/student/accept.blade.php
@@ -3,7 +3,8 @@
 You have been selected by Joint Admission Committee AJ&K.
 
 
-@component('mail::button', ['url' => ''])
+@component('mail::button', ['url' => 'https://mcadmissions.ajk.gov.pk/forgot-password'])
+    Visit Website to check status
 @endcomponent
 
 Thanks,<br>
--- resources/views/mail/student/reject.blade.php
+++ resources/views/mail/student/reject.blade.php
@@ -5,9 +5,6 @@ We can not proceed with your application.
 
 Better luck next time
 
-@component('mail::button', ['url' => ''])
-
-@endcomponent
 
 Thanks,<br>
 {{ config('app.name') }}
